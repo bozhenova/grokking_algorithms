24e5e0eb6113452636e0dd3d43275f529445f2c8
Margo <margo.bozhenova@gmail.com>
2019-11-26 04:54:31 +0300

    Wrote sum of integers with recursion function

--- recursion-prompts-master/src/recursion.js
+++ recursion-prompts-master/src/recursion.js
@@ -17,7 +17,17 @@ var factorial = function(n) {
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
-var sum = function(array) {};
+var sum = function(array) {
+  var tArray = array.slice();
+  if (tArray.length > 1) {
+    return tArray[0] + sum(tArray.slice(1));
+  } else if (tArray.length == 1) {
+    return tArray[0];
+  }
+  return 0;
+};
+
+sum([2, 4, 6]);
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
