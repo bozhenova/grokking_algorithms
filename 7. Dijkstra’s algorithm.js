2b061fe0f89b96314485536376be2f7f06901a02
Margo <margo.bozhenova@gmail.com>
2020-01-08 18:59:16 +0300

    Add JS implementation of Dijkstra's Algorithm

--- 7. Dijkstra’s algorithm.js
+++ 7. Dijkstra’s algorithm.js
@@ -1,24 +1,55 @@
 "use strict";
 
-const graph = {
-  start: {
-    A: 6,
-    B: 2
-  },
-  A: {
-    fin: 1
-  },
-  B: {
-    A: 3,
-    fin: 5
-  },
-  fin: {}
-};
+const graph = {};
+graph["start"] = {};
+graph["start"]["a"] = 6;
+graph["start"]["b"] = 2;
 
-const parents = {
-  A: start,
-  B: start,
-  In: Null
-};
+graph["a"] = {};
+graph["a"]["fin"] = 1;
+graph["b"] = {};
+graph["b"]["a"] = 3;
+graph["b"]["fin"] = 5;
+graph["fin"] = {};
+
+
+const costs = {};
+costs["a"] = 6;
+costs["b"] = 2;
+costs["fin"] = Infinity;
+
+
+const parents = {};
+parents["a"] = "start";
+parents["b"] = "start";
+parents["fin"] = null;
 
 const processed = [];
+
+function findLowestCostNode(costs) {
+  let lowestCost = Infinity;
+  let lowestCostNode = null;
+  for (let node in costs) {
+    let cost = costs[node];
+    if (cost < lowestCost && processed.indexOf(node) === -1) {
+      lowestCost = cost;
+      lowestCostNode = node;
+    }
+  }
+  return lowestCostNode;
+}
+
+let node = findLowestCostNode(costs);
+while (node !== null) {
+  let cost = costs[node];
+  let neighbors = graph[node];
+  for (let n in neighbors) {
+    let newCost = cost + neighbors[n];
+    if (newCost < costs[n]) {
+      costs[n] = newCost;
+      parents[n] = node;
+    }
+  }
+  processed.push(node);
+  node = findLowestCostNode(costs);
+}
