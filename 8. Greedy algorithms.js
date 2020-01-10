e507c8f724b53019ab51897dea131db0139dc8af
Margo <margo.bozhenova@gmail.com>
2020-01-10 10:58:14 +0300

    Add Set cover problem solution

--- 8. Greedy algorithms.js
+++ 8. Greedy algorithms.js
@@ -1,4 +1,27 @@
-Ch 8. Greedy algorithms
-Ch 9. Dynamic programming
-Ch 10. K-nearest neighbors
-Ch 11. Where to go next
+"use strict";
+
+let statesNeeded = new Set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az']);
+
+const stations = {};
+stations['kone'] = new Set(['id', 'nv', 'ut']);
+stations['ktwo'] = new Set(['wa', 'it', 'mt']);
+stations['kthree'] = new Set(['or', 'nv', 'ca']);
+stations['kfour'] = new Set(['nv', 'ut']);
+stations['kfive'] = new Set(['ca', 'az']);
+
+const finalStations = new Set();
+
+while (statesNeeded.size) {
+  let bestStation = null;
+  let statesCovered = new Set();
+  for (let station in stations) {
+    let states = stations[station];
+    let covered = new Set([...statesNeeded].filter(x => states.has(x)));
+    if (covered.size > statesCovered.size) {
+      bestStation = station;
+      statesCovered = covered;
+    }
+  }
+  statesNeeded = new Set([...statesNeeded].filter((x) => !statesCovered.has(x)));
+  finalStations.add(bestStation);
+}
